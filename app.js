55f3128d87e58eec67ec2b5d58cc251a01be290e
Ayush Dwivedi <ayushhh120@gmail.com>
2025-03-19 18:19:11 +0530

    i updated my code

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ function convertSecondsToTime(seconds) {
 
 async function getSongs(folder) {
   currFolder = folder;
-  let a = await fetch(`/${folder}/`);
+  let a = await fetch('/songs/${folder}/info.json');
   let response = await a.text();
   let div = document.createElement("div");
   div.innerHTML = response;
