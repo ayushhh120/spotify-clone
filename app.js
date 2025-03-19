8bb44283e0a7adfbcd7d018b1cde6bb039ea12fe
Ayush Dwivedi <ayushhh120@gmail.com>
2025-03-19 16:52:18 +0530

    i updated my js code according to my albums.json

--- app.js
+++ app.js
@@ -67,40 +67,26 @@ const playMusic = (track, pause = false) => {
 };
 
 async function displayAlbums( ){
-    let a = await fetch(`/songs/`);
-    let response = await a.text();
-    let div = document.createElement("div");
-    div.innerHTML = response;
-    let anchors = div.getElementsByTagName("a")
+    let a = await fetch(`/songs/albums.json`);
+    let albums = await a.json();
     let cardContainer = document.querySelector(".cardContainer")
-    Array.from(anchors).forEach(async e=>{
-      let folder = ""
-        if(e.href.includes("songs/") && !e.href.includes(".htaccess")){
-       folder = e.href.split("/").slice(-1)[0];
-        }
-        if(folder) try{
-          let a = await fetch(`/songs/${folder}/info.json`);
-        let response = await a.json()
-        cardContainer.innerHTML += `<div data-folder="${folder}" class="card">
-            <div class= "play"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="40" height="40">
-              <circle cx="12" cy="12" r="12" fill="#1FDF64"/>
-              <g transform="scale(0.8) translate(3,3)">
-                  <path d="M18.8906 12.846C18.5371 14.189 16.8667 15.138 13.5257 17.0361C10.296 18.8709 8.6812 19.7884 7.37983 19.4196C6.8418 19.2671 6.35159 18.9776 5.95624 18.5787C5 17.6139 5 15.7426 5 12C5 8.2574 5 6.3861 5.95624 5.42132C6.35159 5.02245 6.8418 4.73288 7.37983 4.58042C8.6812 4.21165 10.296 5.12907 13.5257 6.96393C16.8667 8.86197 18.5371 9.811 18.8906 11.154C19.0365 11.7084 19.0365 12.2916 18.8906 12.846Z" stroke="black" stroke-width="1.5" stroke-linejoin="round" fill="black"/>
-              </g>
-          </svg>
-          </div>
-            <img src="/songs/${folder}/cover.jfif">
-            <h4>${response.title}</h4>
-            <p>${response.description}</p>
-          </div>`
-        }
-       catch(error){
-        console.log(`Error fetching info.json for folder: ${folder}`, error);
-       }
-        
+    albums.forEach(album=>{
+                         
+        cardContainer.innerHTML += `<div data-folder="${album.folder}" class="card">
+        <div class= "play"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="40" height="40">
+          <circle cx="12" cy="12" r="12" fill="#1FDF64"/>
+          <g transform="scale(0.8) translate(3,3)">
+              <path d="M18.8906 12.846C18.5371 14.189 16.8667 15.138 13.5257 17.0361C10.296 18.8709 8.6812 19.7884 7.37983 19.4196C6.8418 19.2671 6.35159 18.9776 5.95624 18.5787C5 17.6139 5 15.7426 5 12C5 8.2574 5 6.3861 5.95624 5.42132C6.35159 5.02245 6.8418 4.73288 7.37983 4.58042C8.6812 4.21165 10.296 5.12907 13.5257 6.96393C16.8667 8.86197 18.5371 9.811 18.8906 11.154C19.0365 11.7084 19.0365 12.2916 18.8906 12.846Z" stroke="black" stroke-width="1.5" stroke-linejoin="round" fill="black"/>
+          </g>
+      </svg>
+      </div>
+        <img src="${album.cover}">
+        <h4>${album.title}</h4>
+        <p>${album.description}</p>
+      </div>`
     })
- 
-}
+
+
 
 async function main() {
 
@@ -209,6 +195,5 @@ document.querySelector(".volume>img").addEventListener("click", e=>{
   }
 
 }) 
-
-}
+}}
 main();
